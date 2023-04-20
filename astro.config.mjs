0dbdda5ebde9e233d0e45c0264041e90efbfefde
Jérôme Abel <contact@jeromeabel.net>
2023-04-20 19:50:58 +0200

    up config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:/jeromeabel.github.io',
-  //outDir: './build',
-  integrations: [tailwind()]
+  site: 'https://jeromeabel.github.io',
+  integrations: [tailwind()],
+  outDir: './dist',
+  base: './',
+  build: {
+    assets: 'assets',
+    assetsPrefix: 'https://jeromeabel.github.io'
+  }
 });
