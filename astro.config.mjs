f799751287db29ec7076a981c00f5901f6b1aac1
Jérôme Abel <contact@jeromeabel.net>
2023-04-24 18:19:01 +0200

    up images, nav, content fields

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeromeabel.github.io',
   integrations: [tailwind()],
-  outDir: './dist',
-  base: './',
+  // outDir: './dist',
+  // base: './',
   build: {
     assets: 'assets',
     assetsPrefix: 'https://jeromeabel.github.io'
