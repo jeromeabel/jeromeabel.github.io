20d26a6b93366a295b1fe1d0b9d9dab5b5eda2f7
Jérôme Abel <contact@jeromeabel.net>
2023-04-25 17:28:37 +0200

    up images, linkns -hero-footer, and projects page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeromeabel.github.io',
-  integrations: [tailwind()],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
   // outDir: './dist',
   // base: './',
   build: {
