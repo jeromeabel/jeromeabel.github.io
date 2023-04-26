3c7ab72de3a76a427e56964420498985b269d7f7
Jérôme Abel <contact@jeromeabel.net>
2023-04-26 23:20:58 +0200

    up image, content text and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeromeabel.github.io',
-  integrations: [tailwind(), image({
+  integrations: [tailwind(), mdx(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx()],
-  // outDir: './dist',
-  // base: './',
+  })],
   build: {
     assets: 'assets',
     assetsPrefix: 'https://jeromeabel.github.io'
