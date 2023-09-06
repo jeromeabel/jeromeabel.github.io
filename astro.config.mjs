139b2843cfc0e6915b4c57db3269f08f1c36eb4a
Jérôme Abel <contact@jeromeabel.net>
2023-09-06 11:07:25 +0200

    Add fonts / custom tailwind, add header/main/footer layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeromeabel.github.io', // for new URL
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans', 'sans-serif'],
+        heading: ['Bubbler One', 'sans-serif'],
+      },
+      container: {
+        center: true,
+      },
+    },
+  },
+  plugins: [],
+};
