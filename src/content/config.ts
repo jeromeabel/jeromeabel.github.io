cda86a8f1d5c9b780a8169c64b185305c0416497
Jérôme Abel <contact@jeromeabel.net>
2023-09-21 14:02:44 +0200

    Up Collections: name and add image() feature

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,30 @@
 import { defineCollection, z } from 'astro:content';
 
-export const collections = {
-  work: defineCollection({
-    schema: z.object({
-      title: z.string(),
-      date: z.coerce.date(),
-      img: z.string(),
-	  img_preview: z.string(),
-      description: z.string(),
-      abstract: z.string(),
-      type: z.string(),
-      git: z.string().optional(),
-      website: z.string().optional(),
-	  live: z.string().optional(),
-      stack: z.array(z.string()).optional(),
-      home: z.boolean().optional(),
+const worksCollection = defineCollection({
+  type: 'content',
+  schema: ({image}) => z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    img_preview: image().refine((img) => img.width >= 500, {
+      message: 'Cover image must be at least 500 pixels wide!',
     }),
+    // image: z.object({
+    //   url: z.string(),
+    //   alt: z.string(),
+    // }),
+    img: z.string(),
+    // img_preview: z.string(),
+    description: z.string(),
+    abstract: z.string(),
+    type: z.string(),
+    git: z.string().optional(),
+    website: z.string().optional(),
+    live: z.string().optional(),
+    stack: z.array(z.string()).optional(),
+    home: z.boolean().optional(),
   }),
+});
+
+export const collections = {
+  works: worksCollection,
 };
