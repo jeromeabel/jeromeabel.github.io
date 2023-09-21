37d12b049b045bd174f75cedbad91d4f71c06e19
Jérôme Abel <contact@jeromeabel.net>
2023-09-21 23:28:47 +0200

    Astro config: add build property for assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://jeromeabel.github.io', // for new URL
   integrations: [tailwind()],
+  build: {
+    assets: 'assets',
+    assetsPrefix: 'https://jeromeabel.github.io',
+    // assetsPrefix: 'https://dev.jeromeabel.net'
+  },
 });
