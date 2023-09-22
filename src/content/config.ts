56243dbfced891980ca4cf67e0cc36b64a26387d
Jérôme Abel <contact@jeromeabel.net>
2023-09-22 11:19:46 +0200

    Schema: Add image() to works collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,27 +2,30 @@ import { defineCollection, z } from 'astro:content';
 
 const worksCollection = defineCollection({
   type: 'content',
-  schema: ({image}) => z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-    img_preview: image().refine((img) => img.width >= 500, {
-      message: 'Cover image must be at least 500 pixels wide!',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.coerce.date(),
+      img_preview: image().refine((img) => img.width >= 500, {
+        message: 'Cover image must be at least 500 pixels wide!',
+      }),
+      img: image().refine((img) => img.width >= 500, {
+        message: 'Cover image must be at least 500 pixels wide!',
+      }),
+      // image: z.object({
+      //   url: z.string(),
+      //   alt: z.string(),
+      // }),
+      // img_preview: z.string(),
+      description: z.string(),
+      abstract: z.string(),
+      type: z.string(),
+      git: z.string().optional(),
+      website: z.string().optional(),
+      live: z.string().optional(),
+      stack: z.array(z.string()).optional(),
+      home: z.boolean().optional(),
     }),
-    // image: z.object({
-    //   url: z.string(),
-    //   alt: z.string(),
-    // }),
-    img: z.string(),
-    // img_preview: z.string(),
-    description: z.string(),
-    abstract: z.string(),
-    type: z.string(),
-    git: z.string().optional(),
-    website: z.string().optional(),
-    live: z.string().optional(),
-    stack: z.array(z.string()).optional(),
-    home: z.boolean().optional(),
-  }),
 });
 
 export const collections = {
