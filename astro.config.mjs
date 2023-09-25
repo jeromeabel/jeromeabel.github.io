54008fefc53087ccd0198c265c28c6e07738edfb
Jérôme Abel <contact@jeromeabel.net>
2023-09-25 18:01:32 +0200

    Change website root to test deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeromeabel.github.io', // for new URL
+  // site: 'https://jeromeabel.github.io', // for new URL
+  site: 'dev.jeromeabel.net',
   integrations: [tailwind()],
   build: {
     assets: 'assets',
-    assetsPrefix: 'https://jeromeabel.github.io',
-    // assetsPrefix: 'https://dev.jeromeabel.net'
+    // assetsPrefix: 'https://jeromeabel.github.io',
+    assetsPrefix: 'https://dev.jeromeabel.net'
   },
   markdown: {
     rehypePlugins: [
