76cd300bbcbc6d164d40657d3867f205de977b7f
Jérôme Abel <contact@jeromeabel.net>
2023-10-03 16:06:43 +0200

    Fix: viewTransition and script with event listener "astro:page-load"

--- src/scripts/scrollAnim.ts
+++ src/scripts/scrollAnim.ts
@@ -1,14 +1,18 @@
-const reveals = [...document.querySelectorAll('.reveal')];
+document.addEventListener('astro:page-load', () => {
+  const reveals = [...document.querySelectorAll('.reveal')];
 
-// create observer
-const callbackObserver = (entries: any[]) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('reveal-anim');
-    } 
+  // create observer
+  const callbackObserver = (entries: any[]) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('reveal-anim');
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callbackObserver, {
+    threshold: 0.25,
   });
-};
-const observer = new IntersectionObserver(callbackObserver, { threshold:0.25});
 
-// observe boxes
-reveals.forEach((reveal) => observer.observe(reveal));
+  // observe boxes
+  reveals.forEach((reveal) => observer.observe(reveal));
+});
