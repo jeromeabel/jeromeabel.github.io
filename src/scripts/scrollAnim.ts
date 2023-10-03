3737f844b59df97e84c5981e9e0ac488e1c80614
Jérôme Abel <contact@jeromeabel.net>
2023-10-03 15:57:04 +0200

    Animations: add intersection observer and fix issue with ViewTransition data-astro-reload on Home link

---
+++ src/scripts/scrollAnim.ts
@@ -0,0 +1,14 @@
+const reveals = [...document.querySelectorAll('.reveal')];
+
+// create observer
+const callbackObserver = (entries: any[]) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('reveal-anim');
+    } 
+  });
+};
+const observer = new IntersectionObserver(callbackObserver, { threshold:0.25});
+
+// observe boxes
+reveals.forEach((reveal) => observer.observe(reveal));
