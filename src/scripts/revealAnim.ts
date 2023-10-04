6f50fc9e887ea8de58e26e6c77d31c8c8d19551a
Jérôme Abel <contact@jeromeabel.net>
2023-10-04 09:17:58 +0200

    Reveal Anim, ACCESSIBILITY: classList contains and motion-reduce

--- src/scripts/revealAnim.ts
+++ src/scripts/revealAnim.ts
@@ -1,18 +1,21 @@
 document.addEventListener('astro:page-load', () => {
-  const reveals = [...document.querySelectorAll('.reveal')];
+  const reveals = [...document.querySelectorAll<HTMLElement>('.reveal')];
 
-  // create observer
-  const callbackObserver = (entries: any[]) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('reveal-anim');
-      }
+  if (reveals) {
+    // create observer
+    const callbackObserver = (entries: any[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !entry.target.classList.contains('reveal-anim')) {
+          entry.target.classList.add('reveal-anim');
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callbackObserver, {
+      threshold: 0.25,
     });
-  };
-  const observer = new IntersectionObserver(callbackObserver, {
-    threshold: 0.25,
-  });
 
-  // observe boxes
-  reveals.forEach((reveal) => observer.observe(reveal));
+    // observe boxes
+    reveals.forEach((reveal) => observer.observe(reveal));
+  }
 });
