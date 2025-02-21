d0f1d94bac19a7eacbf5701a102751a8162b8bf8
jeromeabel <j.abel@allo-media.fr>
2025-02-21 23:43:36 +0100

    chore: test whithout dev.jeromeabel.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import tailwind from '@astrojs/tailwind';
 import { defineConfig, passthroughImageService } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dev.jeromeabel.net',
-  integrations: [tailwind()],
-  build: {
-    assets: 'assets',
-    assetsPrefix: 'https://dev.jeromeabel.net',
-  },
+  // site: 'https://dev.jeromeabel.net',
+  // integrations: [tailwind()],
+  // build: {
+  //   assets: 'assets',
+  //   assetsPrefix: 'https://dev.jeromeabel.net',
+  // },
   image: {
     service: passthroughImageService()
   },
