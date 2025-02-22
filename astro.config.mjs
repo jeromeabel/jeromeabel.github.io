89b44d54323c9217a7ce906e69daf2d34da6717b
jeromeabel <j.abel@allo-media.fr>
2025-02-22 15:50:51 +0100

    chore: try netlify adapter for image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
