8667a6171c844c75116085ba1ecfd6ad1a4d6881
jeromeabel <j.abel@allo-media.fr>
2025-02-24 22:40:43 +0100

    feat: add Toggle Them dark mode

---
+++ src/scripts/theme.ts
@@ -0,0 +1,13 @@
+function applyTheme() {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
+document.addEventListener("astro:after-swap", applyTheme);
+applyTheme();
